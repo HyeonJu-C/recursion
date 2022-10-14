749a9cfa112ddade4362ca4c394ee89e1a4c226a
HyeonJu <hhjc017@gmail.com>
2022-10-14 10:58:28 +0900

    bonus challenge - isPalindrome 문제 풀이

--- bonus-challenge.js
+++ bonus-challenge.js
@@ -18,3 +18,26 @@ function reverse(string = '') {
 }
 
 console.log(reverse('hello, world')) //
+
+/**
+문제 2) 
+1. write a function called isPalindrome(a word, phrase, or sequence that reads the same backward as forward, e.g., madam)
+2. which returns true if the string passed to it is a palindrome. 
+ */
+
+function isPalindrome(string = '') {
+  const end = string.length - 1
+  let result
+
+  function helper(index = 0) {
+    if (index <= end) {
+      result = string[index] === string[end - index]
+      result === true && helper(index + 1)
+    }
+    return result
+  }
+  helper(0)
+  return result
+}
+
+console.log(isPalindrome('madam'))
